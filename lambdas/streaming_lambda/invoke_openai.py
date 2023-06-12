9bb93ece25fed7508201f7aa2ea40755648b63b8
rashmipawar921@gmail.com <rashmipawar921@gmail.com>
2023-06-13 02:56:40 +0530

    latest code, template

---
+++ lambdas/streaming_lambda/invoke_openai.py
@@ -0,0 +1,35 @@
+import boto3
+import os
+import openai
+
+class InvokeOpenai:
+    def __init__(self, connectionId):
+        self.ssm = boto3.client("ssm")
+        self.conn = boto3.client("apigatewaymanagementapi", endpoint_url=os.environ["api_endpoint"], region_name=os.environ["region"])
+        self.params = {
+            "Data":"",
+            "ConnectionId": connectionId
+        }
+
+    def read_ssm_parameter(self):
+        openai_key = self.ssm.get_parameter(Name=os.environ["openai_key"],WithDecryption=True)["Parameter"]["Value"]
+        return openai_key
+    
+    def call_openai(self, request, model="gpt-3.5-turbo"):
+        openai.api_key = self.read_ssm_parameter()
+        
+        for resp in openai.ChatCompletion.create(
+                model=model,
+                messages=[
+                        {"role": "system", "content": "You are a helpful assistant."},
+                        {"role": "user", "content": f"{request}"}
+                    ],
+                stream=True,
+                stop=None
+            ):
+            if "content" in resp.choices[0]["delta"]:
+                res = resp.choices[0]["delta"]["content"]
+                response += resp
+                if res != '':
+                    self.params["Data"] = res
+                    self.conn.post_to_connection(**self.params)
--- lambdas/streaming_lambda/lambda_function.py
+++ lambdas/streaming_lambda/lambda_function.py
@@ -1,6 +1,13 @@
 import json
+from invoke_openai import InvokeOpenai
 
 def lambda_handler(event, context):
+    body = json.loads(event["body"])
+    request = body["query"]
+    connectionId = event["requestContext"]["connectionId"]
+    ivk = InvokeOpenai(connectionId)
+    ivk.call_openai(request)
+
     return {
         "statusCode": 200,
         "body": "Success"
