7af75fcbfcfdef012053a475108071f05c2f8878
rashmipawar921@gmail.com <rashmipawar921@gmail.com>
2023-06-13 18:54:10 +0530

    update permission

---
+++ client/client.py
@@ -0,0 +1,34 @@
+import websocket
+import ssl
+
+def on_message(ws, message):
+    print("Received message:", message)
+
+def on_error(ws, error):
+    print("WebSocket error:", error)
+
+def on_close(ws, a, b):
+    print("WebSocket connection closed")
+
+def on_open(ws):
+    print("WebSocket connection opened")
+    # Send a request after the connection is opened
+    request = {
+        "action": "hi",
+        "query":"What is AI?"
+    }
+    ws.send("Hello, WebSocket!")
+
+if __name__ == "__main__":
+    # Specify your WebSocket API Gateway endpoint URL
+    websocket_url = "wss://er7uagkgg4.execute-api.ap-south-1.amazonaws.com/dev"
+
+    # Create a WebSocket connection
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp(websocket_url,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+
+    # Use SSL/TLS for secure WebSocket connection
+    ws.run_forever()
