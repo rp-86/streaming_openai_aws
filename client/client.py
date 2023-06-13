a6bb00f36b5a04b9260362038f272ca025f4236d
rashmipawar921@gmail.com <rashmipawar921@gmail.com>
2023-06-13 20:38:10 +0530

    latest code

--- client/client.py
+++ client/client.py
@@ -1,5 +1,6 @@
 import websocket
 import ssl
+import json
 
 def on_message(ws, message):
     print("Received message:", message)
@@ -14,10 +15,10 @@ def on_open(ws):
     print("WebSocket connection opened")
     # Send a request after the connection is opened
     request = {
-        "action": "hi",
+        "action": "openai",
         "query":"What is AI?"
     }
-    ws.send("Hello, WebSocket!")
+    ws.send(json.dumps(request))
 
 if __name__ == "__main__":
     # Specify your WebSocket API Gateway endpoint URL
@@ -31,4 +32,5 @@ if __name__ == "__main__":
                                 on_close=on_close)
 
     # Use SSL/TLS for secure WebSocket connection
+    ws.on_open = on_open
     ws.run_forever()
